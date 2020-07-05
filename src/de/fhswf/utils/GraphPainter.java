3de80bd4eee738691dc838d5f0b0c384626e3acc
Gin-Wah <ginwah.chau@gmx.de>
2020-07-05 12:58:47 +0200

    lÃ¤nge

--- src/de/fhswf/utils/GraphPainter.java
+++ src/de/fhswf/utils/GraphPainter.java
@@ -21,21 +21,21 @@ public class GraphPainter extends JPanel {
 
 	public Color backgroundColor = new Color(51, 51, 51), mainColor = Color.WHITE, fontColor = Color.BLACK,
 			overlappingEdge = Color.RED, gridColor = new Color(72, 72, 72);
-	
+
 	public int firstKnotenSel = 0, secondKnotenSel = 0;
 	public int size = 65;
 	private FrameSize fSize;
 	private int currentCircle = 0;
 	public EditorMode eM = EditorMode.SelectKnoten;
 	public Map<Knoten, Integer> bfs = null;
-	
+
 	public boolean tV = true;
 	public boolean lV = false;
-	
+
 	public GraphPainter(int kSize) {
 		this(FrameSize.Small, kSize);
 	}
-	
+
 	public GraphPainter(FrameSize fsize, int kSize) {
 		GraphMouseAdapter gMA = new GraphMouseAdapter(this);
 		addMouseListener(gMA);
@@ -57,21 +57,22 @@ public class GraphPainter extends JPanel {
 
 	private void prepGraph(Graph g) {
 		this.graph = g;
-		
-		if(g == null) return;
+
+		if (g == null)
+			return;
 
 		float degreeC = 360f;
-		
+
 		if (g.getAmountKnots() > 1)
 			degreeC = degreeC / g.getAmountKnots();
 
 		for (int i = 0; i < graph.getAmountKnots(); i++) {
-			if(graph.knotList.get(i).pos == null) {
+			if (graph.knotList.get(i).pos == null) {
 				graph.knotList.get(i).pos = addEcke(i * degreeC);
 			}
 		}
 	}
-	
+
 	public void reset() {
 		prepGraph(graph);
 		repaint();
@@ -90,13 +91,12 @@ public class GraphPainter extends JPanel {
 		return new Point(event.getX() + 10, event.getY() + 10);
 	}
 
-	
-	
 	public int getInnerDiameter(Knoten k) {
-		if(k.size != -1)return k.size;
+		if (k.size != -1)
+			return k.size;
 		return (int) size;
 	}
-	
+
 	public int getInnerDiameter() {
 		return (int) size;
 	}
@@ -114,12 +114,13 @@ public class GraphPainter extends JPanel {
 			Knoten k = graph.knotList.get(i - 1);
 			Point oP = k.pos;
 
-			if(k.knotType == KnotenType.Kreis) {
-				double d = Math.hypot(event.getX() - (oP.getX() + getInnerDiameter(k) / 2), event.getY() - (oP.getY() + getInnerDiameter(k) / 2));
+			if (k.knotType == KnotenType.Kreis) {
+				double d = Math.hypot(event.getX() - (oP.getX() + getInnerDiameter(k) / 2),
+						event.getY() - (oP.getY() + getInnerDiameter(k) / 2));
 				if (d < getInnerDiameter(k) / 2)
 					return i;
-			} else if(k.knotType == KnotenType.Quadrat || k.knotType == KnotenType.GerundetesQuadrat) {
-				if(new CustomRect(getInnerDiameter(k), oP).contains(event.getPoint())) {
+			} else if (k.knotType == KnotenType.Quadrat || k.knotType == KnotenType.GerundetesQuadrat) {
+				if (new CustomRect(getInnerDiameter(k), oP).contains(event.getPoint())) {
 					return i;
 				}
 			}
@@ -156,11 +157,12 @@ public class GraphPainter extends JPanel {
 
 	}
 
-	public void resetSelected(){
-		firstKnotenSel = 0; secondKnotenSel = 0;
+	public void resetSelected() {
+		firstKnotenSel = 0;
+		secondKnotenSel = 0;
 		repaint();
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
@@ -168,7 +170,7 @@ public class GraphPainter extends JPanel {
 		g2d.setColor(backgroundColor);
 		g2d.fillRect(0, 0, getWidth(), getHeight());
 		g2d.setColor(mainColor);
-		
+
 		if (graph == null) {
 			g2d.setFont(new Font("Serif", Font.BOLD, 20));
 			int width = g2d.getFontMetrics().stringWidth("Kein Graph ausgewählt!");
@@ -180,24 +182,27 @@ public class GraphPainter extends JPanel {
 			g2d.setFont(new Font("Serif", Font.BOLD, 20));
 			int width = g2d.getFontMetrics().stringWidth("Der gewählte Graph ist zu groß!");
 			g2d.drawString("Der gewählte Graph ist zu groß!", getWidth() / 2 - (width / 2), getHeight() / 2 - 15);
-			
+
 			int width2 = g2d.getFontMetrics().stringWidth("Bitte wenn möglich größeres Fenster öffnen.");
-			g2d.drawString("Bitte wenn möglich größeres Fenster öffnen.", getWidth() / 2 - (width2 / 2), getHeight() / 2 + 10);
+			g2d.drawString("Bitte wenn möglich größeres Fenster öffnen.", getWidth() / 2 - (width2 / 2),
+					getHeight() / 2 + 10);
 			return;
 		}
-		
-		
+
 		double sizeMod = (double) (fSize.maxKnoten) / 30.0;
 		g2d.setColor(gridColor);
-		for(int i = 1; i < 12 * sizeMod; i++) {
-			g2d.drawLine((int) (i * ((double)(getWidth())/12 * (1 / sizeMod))), 0, (int) (i * ((double)(getWidth())/12 * (1 / sizeMod))), getHeight());
+		for (int i = 1; i < 12 * sizeMod; i++) {
+			g2d.drawLine((int) (i * ((double) (getWidth()) / 12 * (1 / sizeMod))), 0,
+					(int) (i * ((double) (getWidth()) / 12 * (1 / sizeMod))), getHeight());
 		}
-		for(int i = 1; i < 12 * sizeMod; i++) {
-			g2d.drawLine(0, (int) (i * ((double)(getHeight())/12 * (1 / sizeMod))), getWidth(), (int) (i * ((double)(getHeight())/12 * (1 / sizeMod))));
+		for (int i = 1; i < 12 * sizeMod; i++) {
+			g2d.drawLine(0, (int) (i * ((double) (getHeight()) / 12 * (1 / sizeMod))), getWidth(),
+					(int) (i * ((double) (getHeight()) / 12 * (1 / sizeMod))));
 		}
-		
-		int fontSize = (int) ((30.0 - (graph.getAmountKnots() * 0.5  * ((30.0 / fSize.maxKnoten)))));
-		g2d.setStroke(new BasicStroke((float) (((2.5f - ((0f + graph.getAmountKnots()) / 20f * (30f / fSize.maxKnoten))) * ((1.0 + size) / 80)))));
+
+		int fontSize = (int) ((30.0 - (graph.getAmountKnots() * 0.5 * ((30.0 / fSize.maxKnoten)))));
+		g2d.setStroke(new BasicStroke((float) (((2.5f - ((0f + graph.getAmountKnots()) / 20f * (30f / fSize.maxKnoten)))
+				* ((1.0 + size) / 80)))));
 		for (Kanten k : graph.edgeList) {
 			g2d.setColor(mainColor);
 			for (Kanten k2 : graph.edgeList) {
@@ -210,27 +215,25 @@ public class GraphPainter extends JPanel {
 				}
 			}
 			drawKante(g2d, k);
-			if(lV) {
+			if (lV) {
 				g2d.setColor(mainColor);
 				g2d.setFont(new Font("Dialog", Font.BOLD, (int) ((fontSize * (1.0 + getInnerDiameter()) / 80) / 1.7)));
-				
+
 				int xDis = (k.k2.pos.x - k.k1.pos.x + getInnerDiameter()) / 2;
 				int yDis = (k.k2.pos.y - k.k1.pos.y + getInnerDiameter()) / 2;
 				double xD = (0.0 + xDis - getInnerDiameter() / 2) / 90.0;// if(xD < 0) xD *=-1;
 				double yD = (0.0 + yDis - getInnerDiameter() / 2) / 90.0;// if(yD < 0) yD *=-1;
 				int xMod = 0, yMod = 0;
-				
-				if((xD < 0 && yD < 0) || xD > 0 && yD > 0) {
+
+				if ((xD < 0 && yD < 0) || xD > 0 && yD > 0) {
 					xMod = 10;
 					yMod = -10;
 				} else {
 					xMod = 10;
 					yMod = 10;
 				}
-				
-				g2d.drawString("" + k.länge,
-						k.k1.pos.x + xDis + xMod,
-						k.k1.pos.y + yDis + yMod);
+
+				g2d.drawString("" + k.l�nge, k.k1.pos.x + xDis + xMod, k.k1.pos.y + yDis + yMod);
 			}
 		}
 		int cP = 0;
@@ -239,47 +242,49 @@ public class GraphPainter extends JPanel {
 			Point p = k.pos;
 			cP++;
 			g2d.setColor(mainColor);
-			if(k.main != null) {
+			if (k.main != null) {
 				g2d.setColor(k.main);
 			}
-			if(k.knotType == KnotenType.Kreis) {
+			if (k.knotType == KnotenType.Kreis) {
 				g2d.fillOval(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k));
-			} else if(k.knotType == KnotenType.Quadrat) {
+			} else if (k.knotType == KnotenType.Quadrat) {
 				g2d.fillRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k));
-			} else if(k.knotType == KnotenType.GerundetesQuadrat) {
-				g2d.fillRoundRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k), getInnerDiameter(k) / 2, getInnerDiameter(k) / 2);
+			} else if (k.knotType == KnotenType.GerundetesQuadrat) {
+				g2d.fillRoundRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k), getInnerDiameter(k) / 2,
+						getInnerDiameter(k) / 2);
 			}
-			
-			if(firstKnotenSel == cP || secondKnotenSel == cP) {
+
+			if (firstKnotenSel == cP || secondKnotenSel == cP) {
 				g2d.setColor(overlappingEdge);
-				if(k.knotType == KnotenType.Kreis) {
+				if (k.knotType == KnotenType.Kreis) {
 					g2d.drawOval(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k));
-				} else if(k.knotType == KnotenType.Quadrat) {
+				} else if (k.knotType == KnotenType.Quadrat) {
 					g2d.drawRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k));
-				} else if(k.knotType == KnotenType.GerundetesQuadrat) {
-					g2d.drawRoundRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k), getInnerDiameter(k) / 2, getInnerDiameter(k) / 2);
+				} else if (k.knotType == KnotenType.GerundetesQuadrat) {
+					g2d.drawRoundRect(p.x, p.y, getInnerDiameter(k), getInnerDiameter(k), getInnerDiameter(k) / 2,
+							getInnerDiameter(k) / 2);
 				}
 			}
 			String knotName = graph.knotList.get(cP - 1).knotName;
-			if(eM == EditorMode.KnotenPos) {
-				if(bfs != null) {
-					if(bfs.containsKey(k)) {
+			if (eM == EditorMode.KnotenPos) {
+				if (bfs != null) {
+					if (bfs.containsKey(k)) {
 						knotName = bfs.get(k) + "";
-						if(knotName.equalsIgnoreCase("-1"))
+						if (knotName.equalsIgnoreCase("-1"))
 							knotName = "∞";
 					}
 				}
 			}
 			if (knotName.length() < 4) {
 				g2d.setColor(fontColor);
-				if(k.font != null) {
+				if (k.font != null) {
 					g2d.setColor(k.font);
 				}
-				if(tV) {
+				if (tV) {
 					Rectangle2D bounds = g2d.getFontMetrics().getStringBounds(knotName, g2d);
 					g2d.drawString(knotName, (int) (p.x + getInnerDiameter(k) / 2 - bounds.getWidth() / 2),
 							(int) (p.y + getInnerDiameter(k) / 2 + bounds.getHeight() / 4));
-				} else if(!knotName.equalsIgnoreCase(graph.knotList.get(cP-1).knotName)) {
+				} else if (!knotName.equalsIgnoreCase(graph.knotList.get(cP - 1).knotName)) {
 					Rectangle2D bounds = g2d.getFontMetrics().getStringBounds(knotName, g2d);
 					g2d.drawString(knotName, (int) (p.x + getInnerDiameter(k) / 2 - bounds.getWidth() / 2),
 							(int) (p.y + getInnerDiameter(k) / 2 + bounds.getHeight() / 4));
