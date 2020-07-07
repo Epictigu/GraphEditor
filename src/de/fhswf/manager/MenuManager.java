c647341392ba6431278fab637317b1da4c51be36
Gin-Wah <ginwah.chau@gmx.de>
2020-07-07 14:39:44 +0200

    save fix

--- src/de/fhswf/manager/MenuManager.java
+++ src/de/fhswf/manager/MenuManager.java
@@ -27,14 +27,16 @@ import de.fhswf.utils.Kanten;
 import de.fhswf.utils.Knoten;
 import de.fhswf.utils.Themes;
 
-public class MenuManager implements ActionListener {
+public class MenuManager implements ActionListener
+{
 
 	private GUI guiInstance;
 	private JMenuBar menuBar;
 	private JMenu menu, edit, settings;
 	private ButtonGroup group;
 
-	public JMenuBar initMenu(GUI f) {
+	public JMenuBar initMenu(GUI f)
+	{
 		guiInstance = f;
 		menuBar = new JMenuBar();
 		menu = new JMenu("File");
@@ -52,7 +54,8 @@ public class MenuManager implements ActionListener {
 		settings = new JMenu("Settings");
 		group = new ButtonGroup();
 
-		for (Themes t : Themes.values()) {
+		for (Themes t : Themes.values())
+		{
 			addThemeButton(t.buttonText, "theme_" + t.name());
 		}
 
@@ -60,7 +63,8 @@ public class MenuManager implements ActionListener {
 		tV.setSelected(true);
 		tV.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0)
+			{
 				guiInstance.k.tV = tV.isSelected();
 				guiInstance.k.repaint();
 			}
@@ -70,7 +74,8 @@ public class MenuManager implements ActionListener {
 		JCheckBoxMenuItem lV = new JCheckBoxMenuItem("Kantenlänge anzeigen");
 		lV.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0)
+			{
 				guiInstance.k.lV = lV.isSelected();
 				guiInstance.k.repaint();
 			}
@@ -84,19 +89,25 @@ public class MenuManager implements ActionListener {
 		return menuBar;
 	}
 
-	private JMenuItem addMenuItem(String text, String actionCommand) {
+	private JMenuItem addMenuItem(String text, String actionCommand)
+	{
 		return addMenuItem(text, actionCommand, "");
 	}
 
-	private JMenuItem addMenuItem(String text, String actionCommand, String iconPath) {
+	private JMenuItem addMenuItem(String text, String actionCommand, String iconPath)
+	{
 		JMenuItem jMI = null;
-		if (!iconPath.equalsIgnoreCase("")) {
-			try {
+		if (!iconPath.equalsIgnoreCase(""))
+		{
+			try
+			{
 				jMI = new JMenuItem(text, new ImageIcon(ImageIO.read(getClass().getResource("/" + iconPath))));
-			} catch (IOException e) {
+			} catch (IOException e)
+			{
 				e.printStackTrace();
 			}
-		} else {
+		} else
+		{
 			jMI = new JMenuItem(text);
 		}
 		jMI.addActionListener(this);
@@ -106,7 +117,8 @@ public class MenuManager implements ActionListener {
 		return jMI;
 	}
 
-	private JRadioButtonMenuItem addThemeButton(String text, String actionCommand) {
+	private JRadioButtonMenuItem addThemeButton(String text, String actionCommand)
+	{
 		JRadioButtonMenuItem theme = new JRadioButtonMenuItem(text);
 		theme.setSelected(false);
 		theme.addActionListener(this);
@@ -117,32 +129,39 @@ public class MenuManager implements ActionListener {
 		return theme;
 	}
 
-	private void openNewFrame(Graph g) {
+	private void openNewFrame(Graph g)
+	{
 		String[] options = { "Klein", "Mittel", "Groß" };
 		int x = JOptionPane.showOptionDialog(null, "Bitte wählen Sie eine Fenstergröße:", "Fenstergröße",
 				JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
-		switch (x) {
-		case 0:
-			Main.openNewFrame(g);
-			break;
-		case 1:
-			Main.openNewFrame(g, FrameSize.Medium);
-			break;
-		case 2:
-			Main.openNewFrame(g, FrameSize.Large);
-			break;
+		switch (x)
+		{
+			case 0:
+				Main.openNewFrame(g);
+				break;
+			case 1:
+				Main.openNewFrame(g, FrameSize.Medium);
+				break;
+			case 2:
+				Main.openNewFrame(g, FrameSize.Large);
+				break;
 		}
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equalsIgnoreCase("selectFile")) {
-			if (new ImportFrame().successful) {
-				if (guiInstance.k.graph.knotList.isEmpty() && guiInstance.k.graph.edgeList.isEmpty()) {
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getActionCommand().equalsIgnoreCase("selectFile"))
+		{
+			if (new ImportFrame().successful)
+			{
+				if (guiInstance.k.graph.knotList.isEmpty() && guiInstance.k.graph.edgeList.isEmpty())
+				{
 					guiInstance.dispose();
 				}
 			}
-		} else if (e.getActionCommand().startsWith("theme")) {
+		} else if (e.getActionCommand().startsWith("theme"))
+		{
 			String theme = e.getActionCommand().split("_")[1];
 			Themes t = Enum.valueOf(Themes.class, theme);
 
@@ -155,17 +174,23 @@ public class MenuManager implements ActionListener {
 
 			guiInstance.eM.edPanel.setBackground(t.mainColor);
 			guiInstance.k.repaint();
-		} else if (e.getActionCommand().equalsIgnoreCase("openWindow")) {
+		} else if (e.getActionCommand().equalsIgnoreCase("openWindow"))
+		{
 			openNewFrame(new Graph(""));
-		} else if (e.getActionCommand().equalsIgnoreCase("exitWindow")) {
+		} else if (e.getActionCommand().equalsIgnoreCase("exitWindow"))
+		{
 			guiInstance.dispose();
-		} else if (e.getActionCommand().equalsIgnoreCase("reset")) {
+		} else if (e.getActionCommand().equalsIgnoreCase("reset"))
+		{
 			guiInstance.k.reset();
-		} else if (e.getActionCommand().equalsIgnoreCase("saveFile")) {
-			if (guiInstance.k.graph.getPath() != null) {
+		} else if (e.getActionCommand().equalsIgnoreCase("saveFile"))
+		{
+			if (!guiInstance.k.graph.getPath().equalsIgnoreCase(""))
+			{
 				FileManager.writeFile(guiInstance.k.graph.getPath(), guiInstance.k.graph, guiInstance.k.size,
 						guiInstance.frameSize);
-			} else {
+			} else
+			{
 				JFileChooser fc = new JFileChooser();
 				FileNameExtensionFilter filter = new FileNameExtensionFilter("GDIDatei", "gdi");
 				fc.setFileFilter(filter);
@@ -175,17 +200,20 @@ public class MenuManager implements ActionListener {
 				// fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
 				int retrival = fc.showSaveDialog(null);
-				if (retrival == JFileChooser.APPROVE_OPTION) {
+				if (retrival == JFileChooser.APPROVE_OPTION)
+				{
 					String path = fc.getSelectedFile().getAbsolutePath();
 
-					if (!path.toLowerCase().endsWith(".gdi")) {
+					if (!path.toLowerCase().endsWith(".gdi"))
+					{
 						path = path + ".gdi";
 					}
 
 					FileManager.writeFile(path, guiInstance.k.graph, guiInstance.k.size, guiInstance.frameSize);
 				}
 			}
-		} else if (e.getActionCommand().equalsIgnoreCase("saveFileAs")) {
+		} else if (e.getActionCommand().equalsIgnoreCase("saveFileAs"))
+		{
 			JFileChooser fc = new JFileChooser();
 			FileNameExtensionFilter filter = new FileNameExtensionFilter("GDIDatei", "gdi");
 			fc.setFileFilter(filter);
@@ -195,23 +223,28 @@ public class MenuManager implements ActionListener {
 			// fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 
 			int retrival = fc.showSaveDialog(null);
-			if (retrival == JFileChooser.APPROVE_OPTION) {
+			if (retrival == JFileChooser.APPROVE_OPTION)
+			{
 				String path = fc.getSelectedFile().getAbsolutePath();
 
-				if (!path.toLowerCase().endsWith(".gdi")) {
+				if (!path.toLowerCase().endsWith(".gdi"))
+				{
 					path = path + ".gdi";
 				}
 
 				guiInstance.k.graph.setPath(path);
 				FileManager.writeFile(path, guiInstance.k.graph, guiInstance.k.size, guiInstance.frameSize);
 			}
-		} else if (e.getActionCommand().equalsIgnoreCase("dataTable")) {
+		} else if (e.getActionCommand().equalsIgnoreCase("dataTable"))
+		{
 			DataTable dt = new DataTable();
-			for (int i = 0; i < guiInstance.k.graph.knotList.size(); i++) {
+			for (int i = 0; i < guiInstance.k.graph.knotList.size(); i++)
+			{
 				Knoten k = guiInstance.k.graph.knotList.get(i);
 				dt.addRowKnoten((i + 1) + "", k.knotName);
 			}
-			for (int i = 0; i < guiInstance.k.graph.edgeList.size(); i++) {
+			for (int i = 0; i < guiInstance.k.graph.edgeList.size(); i++)
+			{
 				Kanten k = guiInstance.k.graph.edgeList.get(i);
 				dt.addRowKanten((i + 1) + "", k.länge + "", (guiInstance.k.graph.getKnotPosInList(k.k1) + 1) + " - "
 						+ (guiInstance.k.graph.getKnotPosInList(k.k2) + 1));
